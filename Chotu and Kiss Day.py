feb81fef539bb133488f94ca27ef68f6908511d6
Abhishek Sadhwani <44032901+sadhwaniabhi@users.noreply.github.com>
2021-07-11 23:30:29 +0530

    Update Chotu and Kiss Day.py

--- Chotu and Kiss Day.py
+++ Chotu and Kiss Day.py
@@ -2,7 +2,7 @@
 #funtion
 def help_chotu(s,l,k):
     for i in range(0,l-1):
-        x=set()
+        x=set()                   #create the set of list which will have all the unique values so that to check chotu can't visit the same place twice
         curr_sum=k-s[i]
         for j in range(i+1,l):
             if (curr_sum-s[j]) in x:
@@ -13,8 +13,8 @@ def help_chotu(s,l,k):
 #inputs 
 number=int(input())    #total no of places
 Si=input()             #Si value of places
-si_array=[]
-for n in Si.split():
+si_array=[]                
+for n in Si.split():          #changing the string value entered by user to list so that we can iterate through it
   si_array.append(int(n))
 
 si_length=len(si_array)
